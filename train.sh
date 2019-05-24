7e33d92451c0e1ccc81f2f784ea78d3ecef6361c
anorthman <940769395@qq.com>
2019-05-24 14:46:19 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -1,13 +1,16 @@
 #! /bin/sh 
 
-fb_cfg=$1
-theta_txt=$2
+model_cfg=$1
+fb_cfg=$2
+theta_txt=$3
+shift 3
+work_dirs=$@
 export CUDA_VISIBLE_DEVICES=2,3 
 python tools/fbnet_train.py \
     --fb_cfg $fb_cfg \
-    --model_cfg configs/model_config/fasterrcnn_str16.py \
+    --model_cfg $model_cfg \
     --gpus 2 \
     --theta_txt $theta_txt \
-    --work_dir ./work_dirs/face/search_fbnet_bottleneckbase3x3 \
+    $work_dirs    
     #--theta_txt theta/epoch_30_end_arch_params.txt \
     #--work_dir ./work_dirs/car/search_fbnet2 \
